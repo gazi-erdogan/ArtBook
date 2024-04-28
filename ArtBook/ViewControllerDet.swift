337db418e09e40b4e30671ee9be0331b2b3343b3
Gazi Erdoğan <gazierdogan@icloud.com>
2024-04-28 22:58:54 +0300

    first commit

---
+++ ArtBook/ViewControllerDet.swift
@@ -0,0 +1,26 @@
+
+
+import UIKit
+
+class ViewControllerDet: UIViewController 
+{
+
+    
+    @IBOutlet weak var imgArtist: UIImageView!
+    
+    
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtArtist: UITextField!
+    @IBOutlet weak var txtYear: UITextField!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
